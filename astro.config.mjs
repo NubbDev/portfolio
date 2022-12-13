a0ffb996e8721439fad3491beccd90bc8405715e
Nubb <giloe.dev@gmail.com>
2022-12-13 09:13:16 -0700

    add netlify deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import netlify from '@astrojs/netlify/functions'
 
 // https://astro.build/config
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), react()]
+  // site: 'https://example.com',
+  integrations: [mdx(), sitemap(), react()],
+  output: 'server',
+  adapter: netlify(),
 });
